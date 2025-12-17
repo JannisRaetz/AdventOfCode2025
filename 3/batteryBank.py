396ab9a35d9ca9743a3135239e8f1c419a862564
Rätz Jannis <jannis.raetz@bgv.de>
2025-12-17 15:50:51 +0100

    Trying to get Day 3/2 to work.

--- 3/batteryBank.py
+++ 3/batteryBank.py
@@ -4,23 +4,44 @@ class Battery:
     def __init__(self, joltage: str):
         self.joltage = joltage
 
-    def get_max_joltage(self) -> int:
-        digit1: int = 0
-        digit1_finished = False
-        digit2: int = 0
+    def get_max_joltage(self, number_of_battery_slots) -> int:
+        digits = {i: 0 for i in range(number_of_battery_slots)}
         for i in range(len(self.joltage)):
             current_digit = int(self.joltage[i])
-            if current_digit > digit1 and not digit1_finished and i < len(self.joltage) - 1:
-                digit1 = current_digit
-                digit2 = 0
-                if digit1 == 9:
-                    digit1_finished = True
-            elif current_digit > digit2:
-                digit2 = current_digit
-                if digit2 == 9:
+            for slot in range(number_of_battery_slots):
+                if len(digits) > slot and digits[slot] == 9:
+                    continue
+                if current_digit > digits[slot] and i + number_of_battery_slots < len(self.joltage) - 1:
+                    digits[slot] = current_digit
+                    for next_slot in range(slot + 1, number_of_battery_slots):
+                        digits[next_slot] = 0
                     break
-        print(int(str(digit1) + str(digit2)))
-        return int(str(digit1) + str(digit2))
+
+            # if i + number_of_battery_slots == len(self.joltage) - 1:
+            #     break
+
+        print(digits)
+        max_joiltage = int("".join(str(x) for x in digits.values()))
+        print(max_joiltage)
+        return int(max_joiltage)
+
+    # def get_max_joltage(self) -> int:
+    #     digit1: int = 0
+    #     digit1_finished = False
+    #     digit2: int = 0
+    #     for i in range(len(self.joltage)):
+    #         current_digit = int(self.joltage[i])
+    #         if current_digit > digit1 and not digit1_finished and i < len(self.joltage) - 1:
+    #             digit1 = current_digit
+    #             digit2 = 0
+    #             if digit1 == 9:
+    #                 digit1_finished = True
+    #         elif current_digit > digit2:
+    #             digit2 = current_digit
+    #             if digit2 == 9:
+    #                 break
+    #     print(int(str(digit1) + str(digit2)))
+    #     return int(str(digit1) + str(digit2))
 
 
 class BatteryBank:
@@ -31,10 +52,10 @@ class BatteryBank:
         for line in batteries_string.splitlines():
             self.batteries.append(Battery(line.strip()))
 
-    def get_total_max_joltage(self) -> int:
+    def get_total_max_joltage(self, number_of_battery_slots) -> int:
         total = 0
         for battery in self.batteries:
-            total += battery.get_max_joltage()
+            total += battery.get_max_joltage(number_of_battery_slots)
         return total
 
 
@@ -42,10 +63,12 @@ def main():
     import sys
     # puzzle_input = sys.stdin.read().strip()
     # with open("mini-input.txt", "r") as f:
-    with open("input.txt", "r") as f:
+    with open("super-mini-input.txt", "r") as f:
+    # with open("input.txt", "r") as f:
         puzzle_input = f.read().strip()
     battery_bank = BatteryBank(puzzle_input)
-    print(battery_bank.get_total_max_joltage())
+    print(battery_bank.get_total_max_joltage(2))
+    # print(battery_bank.get_total_max_joltage(12))
 
 
 if __name__ == "__main__":
